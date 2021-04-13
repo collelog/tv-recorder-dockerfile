88d56dd5e7c0590512acf1a320e5678c3610cef6
collelog <collelog.cavamin@gmail.com>
2021-04-13 13:24:28 +0900

    tvchannels-scan 起動時に存在するchannels.yml退避を追加

--- tvchannels-scan/services.sh
+++ tvchannels-scan/services.sh
@@ -4,5 +4,7 @@ if [ -f "/opt/tvchannels-scan/scanner" ]; then
   chmod +x scanner
   ./scanner
 else
+  mv channels.yml channels.yml_`date +%Y%m%d-%H%M%S` 2>/dev/null || echo "backup channels.yml"
+  mv channels_mirakc.yml channels_mirakc.yml_`date +%Y%m%d-%H%M%S` 2>/dev/null || echo "backup channels_mirakc.yml"
   scan_ch_mirak.sh
 fi
