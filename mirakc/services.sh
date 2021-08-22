2889d6f810e7dea1f9d718fae4d633db8c031519
collelog <collelog.cavamin@gmail.com>
2021-08-23 00:47:05 +0900

    mirakc services.sh調整

--- mirakc/services.sh
+++ mirakc/services.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -eux
 
 if [ -e "/etc/init.d/pcscd" ]; then
-  echo "starting pcscd..."
-  /etc/init.d/pcscd start
+  /etc/init.d/pcscd stop > /dev/null 2>&1 || echo "stoping pcscd..."
+  sleep 2
+  /etc/init.d/pcscd start > /dev/null 2>&1 || echo "starting pcscd..."
 fi
 
 echo "Start mirakc"
